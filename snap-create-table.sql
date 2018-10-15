eb66a04781d63811de48d2233946c5bea075e25a
Anthony Garcia <garcia.anthony577@gmail.com>
2018-10-15 13:01:26 -0600

    snap challenge week 2 in mysql updated

--- snap-create-table.sql
+++ snap-create-table.sql
@@ -1,46 +1,51 @@
 ALTER DATABASE agarcia707 CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
-DROP TABLE IF EXISTS `articleTag`;
-DROP TABLE IF EXISTS tag;
+DROP TABLE IF EXISTS articleTag;
 DROP TABLE IF EXISTS article;
+DROP TABLE IF EXISTS tag;
 DROP TABLE IF EXISTS author;
 
 
 CREATE TABLE author(
 	authorId BINARY(16) NOT NULL,
+	authorAvatarUrl VARCHAR(255),
 	authorActivationToken CHAR(32),
-	authorAvatarUrl BINARY(16) NOT NULL,
 	authorEmail VARCHAR(128) NOT NULL,
 	authorHash CHAR(97) NOT NULL,
-	authorUsername BINARY(16) NOT NULL,
-
+	authorUsername VARCHAR(32) NOT NULL,
 	UNIQUE(authorEmail),
-
+	UNIQUE (authorUsername),
 	PRIMARY KEY(authorId)
 );
 
 
+CREATE TABLE tag(
+	tagId BINARY(16) NOT NULL,
+	tagName VARCHAR(32) NOT NULL,
+	PRIMARY KEY(tagId)
+);
+
+
 CREATE TABLE article(
-	articleId BINARY(32) NOT NULL,
-	articleAuthorId BINARY(32) NOT NULL,
-	articleContent VARCHAR(256) NOT NULL,
+	articleId BINARY(16) NOT NULL,
+	articleAuthorId BINARY(16) NOT NULL,
+	articleContent VARCHAR(40000) NOT NULL,
 	articleDate DATETIME(6) NOT NULL,
-	articleImage VARCHAR(256)
+	articleImage VARCHAR(255),
 
-	FOREIGN KEY(articleAuthorId) REFERENCES author(authorId)
-
+	INDEX(articleAuthorId),
+	FOREIGN KEY(articleAuthorId) REFERENCES author(authorId),
 	PRIMARY KEY(articleId)
 );
 
 
-CREATE TABLE tag(
-	tagId BINARY(32) NOT NULL,
-	tagName VARCHAR(150) NOT NULL
-);
-
-
-CREATE TABLE articleTag(
-	articleTagArticleId BINARY(32) NOT NULL,
-	articleTagTagId BINARY(32) NOT NULL
+CREATE TABLE articleTag (
+	articleTagArticleId BINARY(16),
+	articleTagTagId BINARY(16),
+	INDEX(articleTagArticleId),
+	INDEX(articleTagTagId),
+	FOREIGN KEY(articleTagArticleId) REFERENCES article(articleId),
+	FOREIGN KEY(articleTagTagId) REFERENCES tag(tagId),
+	PRIMARY KEY(articleTagArticleId, articleTagTagId)
 );
 
